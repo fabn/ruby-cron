3e9a1240969fe9a9e84701b666799395b9ffcf11
Fabio Napoleoni <f.napoleoni@gmail.com>
2016-02-02 01:33:18 +0100

    Scheduler improved exposing global logger variable

--- scheduler.rb
+++ scheduler.rb
@@ -4,17 +4,17 @@ require 'bundler/setup'
 Bundler.setup(:default, :production)
 require 'logger'
 
-logger = Logger.new(STDOUT)
+$logger = Logger.new(STDOUT)
 
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.singleton
 
-logger.info 'Rufus scheduler started'
+$logger.info 'Rufus scheduler started'
 
 # Shutdown procedure, wait for running jobs then exit
 shutdown = proc do
   # Using a thread otherwise lock won't work
-  Thread.new { logger.debug 'Scheduler shutdown initiated' }  
+  Thread.new { $logger.debug 'Scheduler shutdown initiated' }  
   scheduler.shutdown(:wait)
 end
 
@@ -22,7 +22,12 @@ end
 trap :INT, &shutdown
 trap :TERM, &shutdown
 
-load 'jobs' if File.exists? 'jobs.rb'
+scheduler.every '3s' do
+  # Report aliveness, - 1 because of self
+  $logger.debug "Scheduler is alive and there are #{scheduler.jobs.size - 1} job(s) scheduled"
+end
+
+load 'jobs.rb' if File.exists? 'jobs.rb'
 
 scheduler.join
-logger.debug 'All jobs have been terminated, exiting'
+$logger.debug 'All jobs have been terminated, exiting'
